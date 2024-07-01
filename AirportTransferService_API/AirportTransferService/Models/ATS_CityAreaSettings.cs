03b2ccd774007f1411967f0bd0852f70ba394e10
Beck Lai <a28282605@gmail.com>
2024-07-02 00:57:58 +0800

    feat 新增行政區域設定

---
+++ AirportTransferService_API/AirportTransferService/Models/ATS_CityAreaSettings.cs
@@ -0,0 +1,451 @@
+﻿using static AirportTransferService.App_Code.Appsettings;
+
+namespace AirportTransferService.Models
+{
+    /// <summary>
+    /// ATS_CityAreaSettings
+    /// </summary>
+    /// <param name="cre_userid"></param>
+    /// <param name="cre_time"></param>
+    /// <param name="upd_userid"></param>
+    /// <param name="upd_time"></param>
+    /// <param name="visible"></param>
+    /// <param name="cas_id"></param>
+    /// <param name="zip"></param>
+    /// <param name="city"></param>
+    /// <param name="area"></param>
+    /// <param name="road"></param>
+    /// <param name="section"></param>
+    public class ATS_CityAreaSettings(
+        string? cre_userid = null,
+        DateTime? cre_time = null,
+        string? upd_userid = null,
+        DateTime? upd_time = null,
+        string? cas_id = null,
+        string? visible = null,
+        string? zip = null,
+        string? city = null,
+        string? area = null,
+        string? road = null,
+        string? section = null)
+    {
+        /// <summary>
+        /// cre_userid
+        /// </summary>
+        public string? cre_userid { get; } = cre_userid;
+
+        /// <summary>
+        /// cre_time
+        /// </summary>
+        public DateTime? cre_time { get; } = cre_time;
+
+        /// <summary>
+        /// upd_userid
+        /// </summary>
+        public string? upd_userid { get; } = upd_userid;
+
+        /// <summary>
+        /// upd_time
+        /// </summary>
+        public DateTime? upd_time { get; } = upd_time;
+
+        /// <summary>
+        /// cas_id
+        /// </summary>
+        [Key]
+        public string? cas_id { get; } = cas_id;
+
+        /// <summary>
+        /// visible
+        /// </summary>
+        public string? visible { get; } = visible;
+
+        /// <summary>
+        /// zip
+        /// </summary>
+        public string? zip { get; } = zip;
+
+        /// <summary>
+        /// city
+        /// </summary>
+        public string? city { get; } = city;
+
+        /// <summary>
+        /// area
+        /// </summary>
+        public string? area { get; } = area;
+
+        /// <summary>
+        /// road
+        /// </summary>
+        public string? road { get; } = road;
+
+        /// <summary>
+        /// section
+        /// </summary>
+        public string? section { get; } = section;
+    }
+
+    /// <summary>
+    /// CreateATS_CityAreaSettingsParam
+    /// </summary>
+    /// <param name="cre_userid"></param>
+    /// <param name="cre_time"></param>
+    /// <param name="visible"></param>
+    /// <param name="zip"></param>
+    /// <param name="city"></param>
+    /// <param name="area"></param>
+    /// <param name="road"></param>
+    /// <param name="section"></param>
+    public class CreateATS_CityAreaSettingsParam(
+        string? cre_userid,
+        DateTime? cre_time,
+        string? visible,
+        string? zip,
+        string? city,
+        string? area,
+        string? road,
+        string? section) : ATS_CityAreaSettings(
+            cre_userid: cre_userid,
+            cre_time: cre_time,
+            visible: visible,
+            zip: zip,
+            city: city,
+            area: area,
+            road: road,
+            section: section)
+    {
+    }
+
+    /// <summary>
+    /// UpdateATS_CityAreaSettingsParam
+    /// </summary>
+    /// <param name="upd_userid"></param>
+    /// <param name="upd_time"></param>
+    /// <param name="cas_id"></param>
+    /// <param name="visible"></param>
+    /// <param name="zip"></param>
+    /// <param name="city"></param>
+    /// <param name="area"></param>
+    /// <param name="road"></param>
+    /// <param name="section"></param>
+    public class UpdateATS_CityAreaSettingsParam(
+        string? upd_userid,
+        DateTime? upd_time,
+        string? cas_id,
+        string? visible = api_string_param_no_pass,
+        string? zip = api_string_param_no_pass,
+        string? city = api_string_param_no_pass,
+        string? area = api_string_param_no_pass,
+        string? road = api_string_param_no_pass,
+        string? section = api_string_param_no_pass) : ATS_CityAreaSettings(
+            upd_userid: upd_userid,
+            upd_time: upd_time,
+            visible: visible,
+            cas_id: cas_id,
+            zip: zip,
+            city: city,
+            area: area,
+            road: road,
+            section: section)
+    {
+    }
+
+    /// <summary>
+    /// SearchATS_CityAreaSettingsParam
+    /// </summary>
+    /// <param name="cas_id"></param>
+    /// <param name="visible"></param>
+    /// <param name="zip"></param>
+    /// <param name="city"></param>
+    /// <param name="area"></param>
+    /// <param name="road"></param>
+    /// <param name="section"></param>
+    /// <param name="page"></param>
+    /// <param name="num_per_page"></param>
+    public class SearchATS_CityAreaSettingsParam(
+        string? cas_id = null,
+        string? visible = null,
+        string? zip = null,
+        string? city = null,
+        string? area = null,
+        string? road = null,
+        string? section = null,
+        int page = 0,
+        int num_per_page = 0)
+    {
+        /// <summary>
+        /// cas_id
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_CityAreaSettings.cas_id")]
+        public string? cas_id { get; } = cas_id;
+
+        /// <summary>
+        /// visible
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_CityAreaSettings.visible")]
+        public string? visible { get; } = visible;
+
+        /// <summary>
+        /// zip
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_CityAreaSettings.zip")]
+        public string? zip { get; } = zip;
+
+        /// <summary>
+        /// city
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_CityAreaSettings.city")]
+        public string? city { get; } = city;
+
+        /// <summary>
+        /// area
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_CityAreaSettings.area")]
+        public string? area { get; } = area;
+
+        /// <summary>
+        /// road
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_CityAreaSettings.road")]
+        public string? road { get; } = road;
+
+        /// <summary>
+        /// section
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_CityAreaSettings.section")]
+        public string? section { get; } = section;
+
+        /// <summary>
+        /// page
+        /// </summary>
+        public int page { get; } = page;
+
+        /// <summary>
+        /// num_per_page
+        /// </summary>
+        public int num_per_page { get; } = num_per_page;
+    }
+
+    /// <summary>
+    /// SearchATS_CityAreaSettingsResult
+    /// </summary>
+    public class SearchATS_CityAreaSettingsResult : IEquatable<SearchATS_CityAreaSettingsResult>
+    {
+        /// <summary>
+        /// cre_userid
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.cre_userid")]
+        public string? cre_userid { get; set; }
+
+        /// <summary>
+        /// cre_time
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.cre_time")]
+        public DateTime? cre_time { get; set; }
+
+        /// <summary>
+        /// upd_userid
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.upd_userid")]
+        public string? upd_userid { get; set; }
+
+        /// <summary>
+        /// upd_time
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.upd_time")]
+        public DateTime? upd_time { get; set; }
+
+        /// <summary>
+        /// cas_id
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.cas_id")]
+        public string? cas_id { get; set; }
+
+        /// <summary>
+        /// visible
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.visible")]
+        public string? visible { get; set; }
+
+        /// <summary>
+        /// zip
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.zip")]
+        public string? zip { get; set; }
+
+        /// <summary>
+        /// city
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.city")]
+        public string? city { get; set; }
+
+        /// <summary>
+        /// area
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.area")]
+        public string? area { get; set; }
+
+        /// <summary>
+        /// road
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.road")]
+        public string? road { get; set; }
+
+        /// <summary>
+        /// section
+        /// </summary>
+        [SQLSource("ATS_CityAreaSettings.section")]
+        public string? section { get; set; }
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="searchATS_CityAreaSettingsResult"></param>
+        /// <returns></returns>
+        public bool Equals(SearchATS_CityAreaSettingsResult? searchATS_CityAreaSettingsResult)
+        {
+            if (searchATS_CityAreaSettingsResult is null) return false;
+
+            if (Object.ReferenceEquals(this, searchATS_CityAreaSettingsResult)) return true;
+
+            return
+                cre_userid == searchATS_CityAreaSettingsResult.cre_userid &&
+                cre_time == searchATS_CityAreaSettingsResult.cre_time &&
+                upd_userid == searchATS_CityAreaSettingsResult.upd_userid &&
+                upd_time == searchATS_CityAreaSettingsResult.upd_time &&
+                cas_id == searchATS_CityAreaSettingsResult.cas_id &&
+                visible == searchATS_CityAreaSettingsResult.visible &&
+                zip == searchATS_CityAreaSettingsResult.zip &&
+                city == searchATS_CityAreaSettingsResult.city &&
+                area == searchATS_CityAreaSettingsResult.area &&
+                road == searchATS_CityAreaSettingsResult.road &&
+                section == searchATS_CityAreaSettingsResult.section;
+        }
+    }
+
+    #region API
+    /// <summary>
+    /// ATS_CityAreaSettingsCreate
+    /// </summary>
+    public class ATS_CityAreaSettingsCreate
+    {
+        /// <summary>
+        /// 是否可見
+        /// </summary>
+        [Display(Name = "是否可見"), YN]
+        public string? visible { get; set; } = "Y";
+
+        /// <summary>
+        /// 郵遞區號
+        /// </summary>
+        [Display(Name = "郵遞區號")]
+        public string? zip { get; set; } = "";
+
+        /// <summary>
+        /// 城市
+        /// </summary>
+        [Display(Name = "城市"), Required(ErrorMessage = "請輸入{0}")]
+        public string? city { get; set; } = "";
+
+        /// <summary>
+        /// 區域
+        /// </summary>
+        [Display(Name = "區域"), Required(ErrorMessage = "請輸入{0}")]
+        public string? area { get; set; } = "";
+
+        /// <summary>
+        /// 路
+        /// </summary>
+        [Display(Name = "路")]
+        public string? road { get; set; } = "";
+
+        /// <summary>
+        /// 段
+        /// </summary>
+        [Display(Name = "段")]
+        public string? section { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_CityAreaSettingsUpdate
+    /// </summary>
+    public class ATS_CityAreaSettingsUpdate : ATS_CityAreaSettingsCreate
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Required(ErrorMessage = "請輸入{0}")]
+        [Display(Name = "編號")]
+        public string? cas_id { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_CityAreaSettingsSearch
+    /// </summary>
+    public class ATS_CityAreaSettingsSearch : ATS_CityAreaSettingsCreate
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號")]
+        public string? cas_id { get; set; } = "";
+
+        /// <summary>
+        /// 頁碼
+        /// </summary>
+        [Display(Name = "頁碼")]
+        public int page { get; set; } = 0;
+
+        /// <summary>
+        /// 一頁幾筆
+        /// </summary>
+        [Display(Name = "一頁幾筆")]
+        public int num_per_page { get; set; } = 10;
+
+        /// <summary>
+        /// 是否匯出
+        /// </summary>
+        [YN]
+        [Display(Name = "是否匯出")]
+        public string excel { get; set; } = "N";
+
+        /// <summary>
+        /// 城市
+        /// </summary>
+        [Display(Name = "城市")]
+        public new string? city { get; set; } = "";
+
+        /// <summary>
+        /// 區域
+        /// </summary>
+        [Display(Name = "區域")]
+        public new string? area { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_CityAreaSettingsSearchResult
+    /// </summary>
+    public class ATS_CityAreaSettingsSearchResponse : ATS_CityAreaSettingsCreate
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號")]
+        public string? cas_id { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_CityAreaSettingsDelete
+    /// </summary>
+    public class ATS_CityAreaSettingsDelete
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Required(ErrorMessage = "請輸入{0}")]
+        [Display(Name = "編號")]
+        public string? cas_id { get; set; } = "";
+    }
+    #endregion
+}
---
+++ AirportTransferService_API/AirportTransferService/Services/IATS_CityAreaSettings.cs
@@ -0,0 +1,198 @@
+﻿namespace AirportTransferService.Services
+{
+    /// <summary>
+    /// IATS_CityAreaSettings
+    /// </summary>
+    public interface IATS_CityAreaSettings
+    {
+        /// <summary>
+        /// CreateATS_CityAreaSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        string CreateATS_CityAreaSettings(CreateATS_CityAreaSettingsParam param);
+
+        /// <summary>
+        /// SearchATS_CityAreaSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <param name="columns"></param>
+        /// <param name="sort_columns"></param>
+        /// <param name="page_count"></param>
+        /// <returns></returns>
+        List<SearchATS_CityAreaSettingsResult> SearchATS_CityAreaSettings(SearchATS_CityAreaSettingsParam param, List<string> columns, List<SQL.SQLOrder_obj> sort_columns, out int page_count);
+
+        /// <summary>
+        /// UpdateATS_CityAreaSettings
+        /// </summary>
+        /// <param name="param"></param>
+        void UpdateATS_CityAreaSettings(UpdateATS_CityAreaSettingsParam param);
+
+        /// <summary>
+        /// DeleteATS_CityAreaSettings
+        /// </summary>
+        /// <param name="cas_id"></param>
+        void DeleteATS_CityAreaSettings(string cas_id);
+    }
+
+    /// <summary>
+    /// IATS_CityAreaSettings_IMPL
+    /// </summary>
+    public class IATS_CityAreaSettings_IMPL : IATS_CityAreaSettings
+    {
+        /// <summary>
+        /// _config
+        /// </summary>
+        public readonly IConfiguration _config;
+        private readonly string strConn = "";
+
+        /// <summary>
+        /// IATS_CityAreaSettings_IMPL
+        /// </summary>
+        /// <param name="config"></param>
+        public IATS_CityAreaSettings_IMPL(IConfiguration config)
+        {
+            _config = config;
+            strConn = _config["sql_conn"];
+        }
+
+        /// <summary>
+        /// CreateATS_CityAreaSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public string CreateATS_CityAreaSettings(CreateATS_CityAreaSettingsParam param)
+        {
+            SQL.GenerateSQLCreateQuery(param, out string str_column, out string str_value, false);
+
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                ResultObject<string> get_auto_res = SQL.GetAutoNumber(myConn, "ATS_CityAreaSettings", "", 99999999, false);
+                if (!get_auto_res.success) throw new Exception(message: "取得流水號失敗");
+                string cas_id = get_auto_res.data ?? "";
+                if (string.IsNullOrEmpty(cas_id)) throw new Exception(message: "Empty Key");
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = $@"
+                    insert into ATS_CityAreaSettings({str_column})
+                    values({str_value});";
+                    myCommand.Parameters.AddWithValue("@cas_id", cas_id);
+                    foreach (var property in param.GetType().GetProperties())
+                    {
+                        if (property.GetValue(param) != null) myCommand.Parameters.AddWithValue($"@{property.Name}", property.GetValue(param));
+                    }
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+
+                return cas_id;
+            }
+        }
+
+        /// <summary>
+        /// SearchATS_CityAreaSettmings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <param name="columns"></param>
+        /// <param name="sort_columns"></param>
+        /// <param name="page_count"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public List<SearchATS_CityAreaSettingsResult> SearchATS_CityAreaSettings(SearchATS_CityAreaSettingsParam param, List<string> columns, List<SQL.SQLOrder_obj> sort_columns, out int page_count)
+        {
+            if (!SQL.CheckSearchColumn<SearchATS_CityAreaSettingsResult>(columns)) throw new Exception(message: "Search Column Not Exists");
+
+            DataTable dt = new();
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                string strSql = $@"
+                select {SQL.GenerateSQLSelectQuery<SearchATS_CityAreaSettingsResult>(columns)}
+                from ATS_CityAreaSettings
+                where 1=1
+                {SQL.GenerateSQLWhereQuery(param)}
+                {SQL.GenerateSQLOrderQuery<SearchATS_CityAreaSettingsResult>(sort_columns, "order by ATS_CityAreaSettings.zip desc")}
+                {(param.page > 0 ? "offset((@page-1)) * @num_per_page ROWS fetch next @num_per_page ROWS only;" : "")}";
+
+                dt = SQL.GenerateSQLSelectResult(param, strSql, myConn, out page_count);
+            }
+
+            List<SearchATS_CityAreaSettingsResult> result = [];
+            foreach (DataRow dr in dt.Rows)
+            {
+                result.Add(new SearchATS_CityAreaSettingsResult
+                {
+                    cre_userid = dt.Columns.Contains("cre_userid") ? dr["cre_userid"].ToString() : null,
+                    cre_time = dt.Columns.Contains("cre_time") ? dr.Field<DateTime?>("cre_time") : null,
+                    upd_userid = dt.Columns.Contains("upd_userid") ? dr["upd_userid"].ToString() : null,
+                    upd_time = dt.Columns.Contains("upd_time") ? dr.Field<DateTime?>("upd_time") : null,
+                    cas_id = dt.Columns.Contains("cas_id") ? dr["cas_id"].ToString() : null,
+                    visible = dt.Columns.Contains("visible") ? dr["visible"].ToString() : null,
+                    zip = dt.Columns.Contains("zip") ? dr["zip"].ToString() : null,
+                    city = dt.Columns.Contains("city") ? dr["city"].ToString() : null,
+                    area = dt.Columns.Contains("area") ? dr["area"].ToString() : null,
+                    road = dt.Columns.Contains("road") ? dr["road"].ToString() : null,
+                    section = dt.Columns.Contains("section") ? dr["section"].ToString() : null
+                });
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// UpdateATS_CityAreaSettings
+        /// </summary>
+        /// <param name="param"></param>
+        public void UpdateATS_CityAreaSettings(UpdateATS_CityAreaSettingsParam param)
+        {
+            SQL.GenerateSQLUpdateQuery(param, out string str);
+            SQL.GenerateSQLColumnLogQuery(param, "ATS_CityAreaSettings", out string column_log_output_str, out string column_log_insert_str, out string tmp_table_create_str);
+
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = $@"
+                    {tmp_table_create_str}
+                    update ATS_CityAreaSettings
+                    {str}
+                    {column_log_output_str}
+                    where cas_id=@cas_id
+                    {column_log_insert_str}";
+                    foreach (var property in param.GetType().GetProperties())
+                    {
+                        myCommand.Parameters.AddWithValue($"@{property.Name}", property.GetValue(param) ?? DBNull.Value);
+                    }
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+            }
+        }
+
+        /// <summary>
+        /// DeleteATS_CityAreaSettings
+        /// </summary>
+        /// <param name="cas_id"></param>
+        public void DeleteATS_CityAreaSettings(string cas_id)
+        {
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = @"delete from ATS_CityAreaSettings where cas_id=@cas_id;";
+                    myCommand.Parameters.AddWithValue("@cas_id", cas_id);
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+            }
+        }
+    }
+}

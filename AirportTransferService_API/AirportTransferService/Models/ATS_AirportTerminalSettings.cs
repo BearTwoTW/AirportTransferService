6e2dd99d68e472bf397b294502c6c556d246fc68
Beck Lai <a28282605@gmail.com>
2024-07-05 00:09:10 +0800

    feat 新增機場設定

---
+++ AirportTransferService_API/AirportTransferService/Models/ATS_AirportTerminalSettings.cs
@@ -0,0 +1,347 @@
+﻿using static AirportTransferService.App_Code.Appsettings;
+
+namespace AirportTransferService.Models
+{
+    /// <summary>
+    /// ATS_AirportTerminalSettings
+    /// </summary>
+    /// <param name="cre_userid"></param>
+    /// <param name="cre_time"></param>
+    /// <param name="upd_userid"></param>
+    /// <param name="upd_time"></param>
+    /// <param name="ats_id"></param>
+    /// <param name="visible"></param>
+    /// <param name="airport"></param>
+    /// <param name="terminal"></param>
+    public class ATS_AirportTerminalSettings(
+        string? cre_userid = null,
+        DateTime? cre_time = null,
+        string? upd_userid = null,
+        DateTime? upd_time = null,
+        string? ats_id = null,
+        string? visible = null,
+        string? airport = null,
+        string? terminal = null)
+    {
+        /// <summary>
+        /// cre_userid
+        /// </summary>
+        public string? cre_userid { get; } = cre_userid;
+
+        /// <summary>
+        /// cre_time
+        /// </summary>
+        public DateTime? cre_time { get; } = cre_time;
+
+        /// <summary>
+        /// upd_userid
+        /// </summary>
+        public string? upd_userid { get; } = upd_userid;
+
+        /// <summary>
+        /// upd_time
+        /// </summary>
+        public DateTime? upd_time { get; } = upd_time;
+
+        /// <summary>
+        /// ats_id
+        /// </summary>
+        [Key]
+        public string? ats_id { get; } = ats_id;
+
+        /// <summary>
+        /// visible
+        /// </summary>
+        public string? visible { get; } = visible;
+
+        /// <summary>
+        /// airport
+        /// </summary>
+        public string? airport { get; } = airport;
+
+        /// <summary>
+        /// terminal
+        /// </summary>
+        public string? terminal { get; } = terminal;
+    }
+
+    /// <summary>
+    /// CreateATS_AirportTerminalSettingsParam
+    /// </summary>
+    /// <param name="cre_userid"></param>
+    /// <param name="cre_time"></param>
+    /// <param name="visible"></param>
+    /// <param name="airport"></param>
+    /// <param name="terminal"></param>
+    public class CreateATS_AirportTerminalSettingsParam(
+        string? cre_userid,
+        DateTime? cre_time,
+        string? visible,
+        string? airport,
+        string? terminal) : ATS_AirportTerminalSettings(
+            cre_userid: cre_userid,
+            cre_time: cre_time,
+            visible: visible,
+            airport: airport,
+            terminal: terminal)
+    {
+    }
+
+    /// <summary>
+    /// UpdateATS_AirportTerminalSettingsParam
+    /// </summary>
+    /// <param name="upd_userid"></param>
+    /// <param name="upd_time"></param>
+    /// <param name="ats_id"></param>
+    /// <param name="visible"></param>
+    /// <param name="airport"></param>
+    /// <param name="terminal"></param>
+    public class UpdateATS_AirportTerminalSettingsParam(
+        string? upd_userid,
+        DateTime? upd_time,
+        string? ats_id,
+        string? visible = api_string_param_no_pass,
+        string? airport = api_string_param_no_pass,
+        string? terminal = api_string_param_no_pass) : ATS_AirportTerminalSettings(
+            upd_userid: upd_userid,
+            upd_time: upd_time,
+            ats_id: ats_id,
+            visible: visible,
+            airport: airport,
+            terminal: terminal)
+    {
+    }
+
+    /// <summary>
+    /// SearchATS_AirportTerminalSettingsParam
+    /// </summary>
+    /// <param name="ats_id"></param>
+    /// <param name="visible"></param>
+    /// <param name="airport"></param>
+    /// <param name="terminal"></param>
+    /// <param name="page"></param>
+    /// <param name="num_per_page"></param>
+    public class SearchATS_AirportTerminalSettingsParam(
+        string? ats_id = null,
+        string? visible = null,
+        string? airport = null,
+        string? terminal = null,
+        int page = 0,
+        int num_per_page = 0)
+    {
+        /// <summary>
+        /// ats_id
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_AirportTerminalSettings.ats_id")]
+        public string? ats_id { get; } = ats_id;
+
+        /// <summary>
+        /// visible
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_AirportTerminalSettings.visible")]
+        public string? visible { get; } = visible;
+
+        /// <summary>
+        /// airport
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_AirportTerminalSettings.airport")]
+        public string? airport { get; } = airport;
+
+        /// <summary>
+        /// terminal
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_AirportTerminalSettings.terminal")]
+        public string? terminal { get; } = terminal;
+
+        /// <summary>
+        /// page
+        /// </summary>
+        public int page { get; } = page;
+
+        /// <summary>
+        /// num_per_page
+        /// </summary>
+        public int num_per_page { get; } = num_per_page;
+    }
+
+    /// <summary>
+    /// SearchATS_AirportTerminalSettingsResult
+    /// </summary>
+    public class SearchATS_AirportTerminalSettingsResult : IEquatable<SearchATS_AirportTerminalSettingsResult>
+    {
+        /// <summary>
+        /// cre_userid
+        /// </summary>
+        [SQLSource("ATS_AirportTerminalSettings.cre_userid")]
+        public string? cre_userid { get; set; }
+
+        /// <summary>
+        /// cre_time
+        /// </summary>
+        [SQLSource("ATS_AirportTerminalSettings.cre_time")]
+        public DateTime? cre_time { get; set; }
+
+        /// <summary>
+        /// upd_userid
+        /// </summary>
+        [SQLSource("ATS_AirportTerminalSettings.upd_userid")]
+        public string? upd_userid { get; set; }
+
+        /// <summary>
+        /// upd_time
+        /// </summary>
+        [SQLSource("ATS_AirportTerminalSettings.upd_time")]
+        public DateTime? upd_time { get; set; }
+
+        /// <summary>
+        /// ats_id
+        /// </summary>
+        [SQLSource("ATS_AirportTerminalSettings.ats_id")]
+        public string? ats_id { get; set; }
+
+        /// <summary>
+        /// visible
+        /// </summary>
+        [SQLSource("ATS_AirportTerminalSettings.visible")]
+        public string? visible { get; set; }
+
+        /// <summary>
+        /// airport
+        /// </summary>
+        [SQLSource("ATS_AirportTerminalSettings.airport")]
+        public string? airport { get; set; }
+
+        /// <summary>
+        /// terminal
+        /// </summary>
+        [SQLSource("ATS_AirportTerminalSettings.terminal")]
+        public string? terminal { get; set; }
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="searchATS_AirportTerminalSettingsResult"></param>
+        /// <returns></returns>
+        public bool Equals(SearchATS_AirportTerminalSettingsResult? searchATS_AirportTerminalSettingsResult)
+        {
+            if (searchATS_AirportTerminalSettingsResult is null) return false;
+
+            if (Object.ReferenceEquals(this, searchATS_AirportTerminalSettingsResult)) return true;
+
+            return
+                cre_userid == searchATS_AirportTerminalSettingsResult.cre_userid &&
+                cre_time == searchATS_AirportTerminalSettingsResult.cre_time &&
+                upd_userid == searchATS_AirportTerminalSettingsResult.upd_userid &&
+                upd_time == searchATS_AirportTerminalSettingsResult.upd_time &&
+                ats_id == searchATS_AirportTerminalSettingsResult.ats_id &&
+                visible == searchATS_AirportTerminalSettingsResult.visible &&
+                airport == searchATS_AirportTerminalSettingsResult.airport &&
+                terminal == searchATS_AirportTerminalSettingsResult.terminal;
+        }
+    }
+
+    #region API
+    /// <summary>
+    /// ATS_AirportTerminalSettingsCreate
+    /// </summary>
+    public class ATS_AirportTerminalSettingsCreate
+    {
+        /// <summary>
+        /// 是否可見
+        /// </summary>
+        [Display(Name = "是否可見")]
+        public string? visible { get; set; } = "n";
+
+        /// <summary>
+        /// 機場
+        /// </summary>
+        [Display(Name = "機場"), Required(ErrorMessage = "請輸入{0}")]
+        public string? airport { get; set; } = "";
+
+        /// <summary>
+        /// 航廈
+        /// </summary>
+        [Display(Name = "航廈"), Required(ErrorMessage = "請輸入{0}")]
+        public string? terminal { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_AirportTerminalSettingsUpdate
+    /// </summary>
+    public class ATS_AirportTerminalSettingsUpdate : ATS_AirportTerminalSettingsCreate
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號"), Required(ErrorMessage = "請輸入{0}")]
+        public string? ats_id { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_AirportTerminalSettingsSearch
+    /// </summary>
+    public class ATS_AirportTerminalSettingsSearch : ATS_AirportTerminalSettingsCreate
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號")]
+        public string? ats_id { get; set; } = "";
+
+        /// <summary>
+        /// 頁碼
+        /// </summary>
+        [Display(Name = "頁碼")]
+        public int page { get; set; } = 0;
+
+        /// <summary>
+        /// 一頁幾筆
+        /// </summary>
+        [Display(Name = "一頁幾筆")]
+        public int num_per_page { get; set; } = 10;
+
+        /// <summary>
+        /// 是否匯出
+        /// </summary>
+        [Display(Name = "是否匯出"), YN]
+        public string excel { get; set; } = "N";
+
+        /// <summary>
+        /// 機場
+        /// </summary>
+        [Display(Name = "機場")]
+        public new string? airport { get; set; } = "";
+
+        /// <summary>
+        /// 航廈
+        /// </summary>
+        [Display(Name = "航廈")]
+        public new string? terminal { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_AirportTerminalSettingsSearchResult
+    /// </summary>
+    public class ATS_AirportTerminalSettingsSearchResponse : ATS_AirportTerminalSettingsCreate
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號")]
+        public string? ats_id { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_AirportTerminalSettingsDelete
+    /// </summary>
+    public class ATS_AirportTerminalSettingsDelete
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Required(ErrorMessage = "請輸入{0}")]
+        [Display(Name = "編號")]
+        public string? ats_id { get; set; } = "";
+    }
+    #endregion
+}
---
+++ AirportTransferService_API/AirportTransferService/Services/IATS_AirportTerminalSettings.cs
@@ -0,0 +1,195 @@
+﻿namespace AirportTransferService.Services
+{
+    /// <summary>
+    /// IATS_AirportTerminalSettings
+    /// </summary>
+    public interface IATS_AirportTerminalSettings
+    {
+        /// <summary>
+        /// CreateATS_AirportTerminalSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        string CreateATS_AirportTerminalSettings(CreateATS_AirportTerminalSettingsParam param);
+
+        /// <summary>
+        /// SearchATS_AirportTerminalSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <param name="columns"></param>
+        /// <param name="sort_columns"></param>
+        /// <param name="page_count"></param>
+        /// <returns></returns>
+        List<SearchATS_AirportTerminalSettingsResult> SearchATS_AirportTerminalSettings(SearchATS_AirportTerminalSettingsParam param, List<string> columns, List<SQL.SQLOrder_obj> sort_columns, out int page_count);
+
+        /// <summary>
+        /// UpdateATS_AirportTerminalSettings
+        /// </summary>
+        /// <param name="param"></param>
+        void UpdateATS_AirportTerminalSettings(UpdateATS_AirportTerminalSettingsParam param);
+
+        /// <summary>
+        /// DeleteATS_AirportTerminalSettings
+        /// </summary>
+        /// <param name="ats_id"></param>
+        void DeleteATS_AirportTerminalSettings(string ats_id);
+    }
+
+    /// <summary>
+    /// IATS_AirportTerminalSettings_IMPL
+    /// </summary>
+    public class IATS_AirportTerminalSettings_IMPL : IATS_AirportTerminalSettings
+    {
+        /// <summary>
+        /// _config
+        /// </summary>
+        public readonly IConfiguration _config;
+        private readonly string strConn = "";
+
+        /// <summary>
+        /// IATS_AirportTerminalSettings_IMPL
+        /// </summary>
+        /// <param name="config"></param>
+        public IATS_AirportTerminalSettings_IMPL(IConfiguration config)
+        {
+            _config = config;
+            strConn = _config["sql_conn"];
+        }
+
+        /// <summary>
+        /// CreateATS_AirportTerminalSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public string CreateATS_AirportTerminalSettings(CreateATS_AirportTerminalSettingsParam param)
+        {
+            SQL.GenerateSQLCreateQuery(param, out string str_column, out string str_value, false);
+
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                ResultObject<string> get_auto_res = SQL.GetAutoNumber(myConn, "ATS_AirportTerminalSettings", "", 99999, false);
+                if (!get_auto_res.success) throw new Exception(message: "取得流水號失敗");
+                string ats_id = get_auto_res.data ?? "";
+                if (string.IsNullOrEmpty(ats_id)) throw new Exception(message: "Empty Key");
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = $@"
+                    insert into ATS_AirportTerminalSettings({str_column})
+                    values({str_value});";
+                    myCommand.Parameters.AddWithValue("@ats_id", ats_id);
+                    foreach (var property in param.GetType().GetProperties())
+                    {
+                        if (property.GetValue(param) != null) myCommand.Parameters.AddWithValue($"@{property.Name}", property.GetValue(param));
+                    }
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+
+                return ats_id;
+            }
+        }
+
+        /// <summary>
+        /// SearchATS_AirportTerminalSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <param name="columns"></param>
+        /// <param name="sort_columns"></param>
+        /// <param name="page_count"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public List<SearchATS_AirportTerminalSettingsResult> SearchATS_AirportTerminalSettings(SearchATS_AirportTerminalSettingsParam param, List<string> columns, List<SQL.SQLOrder_obj> sort_columns, out int page_count)
+        {
+            if (!SQL.CheckSearchColumn<SearchATS_AirportTerminalSettingsResult>(columns)) throw new Exception(message: "Search Column Not Exists");
+
+            DataTable dt = new();
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                string strSql = $@"
+                select {SQL.GenerateSQLSelectQuery<SearchATS_AirportTerminalSettingsResult>(columns)}
+                from ATS_AirportTerminalSettings
+                where 1=1
+                {SQL.GenerateSQLWhereQuery(param)}
+                {SQL.GenerateSQLOrderQuery<SearchATS_AirportTerminalSettingsResult>(sort_columns, "order by ATS_AirportTerminalSettings.ats_id desc")}
+                {(param.page > 0 ? "offset((@page-1)) * @num_per_page ROWS fetch next @num_per_page ROWS only;" : "")}";
+
+                dt = SQL.GenerateSQLSelectResult(param, strSql, myConn, out page_count);
+            }
+
+            List<SearchATS_AirportTerminalSettingsResult> result = [];
+            foreach (DataRow dr in dt.Rows)
+            {
+                result.Add(new SearchATS_AirportTerminalSettingsResult
+                {
+                    cre_userid = dt.Columns.Contains("cre_userid") ? dr["cre_userid"].ToString() : null,
+                    cre_time = dt.Columns.Contains("cre_time") ? dr.Field<DateTime?>("cre_time") : null,
+                    upd_userid = dt.Columns.Contains("upd_userid") ? dr["upd_userid"].ToString() : null,
+                    upd_time = dt.Columns.Contains("upd_time") ? dr.Field<DateTime?>("upd_time") : null,
+                    ats_id = dt.Columns.Contains("ats_id") ? dr["ats_id"].ToString() : null,
+                    visible = dt.Columns.Contains("visible") ? dr["visible"].ToString() : null,
+                    airport = dt.Columns.Contains("airport") ? dr["airport"].ToString() : null,
+                    terminal = dt.Columns.Contains("terminal") ? dr["terminal"].ToString() : null
+                });
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// UpdateATS_AirportTerminalSettings
+        /// </summary>
+        /// <param name="param"></param>
+        public void UpdateATS_AirportTerminalSettings(UpdateATS_AirportTerminalSettingsParam param)
+        {
+            SQL.GenerateSQLUpdateQuery(param, out string str);
+            SQL.GenerateSQLColumnLogQuery(param, "ATS_AirportTerminalSettings", out string column_log_output_str, out string column_log_insert_str, out string tmp_table_create_str);
+
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = $@"
+                    {tmp_table_create_str}
+                    update ATS_AirportTerminalSettings
+                    {str}
+                    {column_log_output_str}
+                    where ats_id=@ats_id
+                    {column_log_insert_str}";
+                    foreach (var property in param.GetType().GetProperties())
+                    {
+                        myCommand.Parameters.AddWithValue($"@{property.Name}", property.GetValue(param) ?? DBNull.Value);
+                    }
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+            }
+        }
+
+        /// <summary>
+        /// DeleteATS_AirportTerminalSettings
+        /// </summary>
+        /// <param name="ats_id"></param>
+        public void DeleteATS_AirportTerminalSettings(string ats_id)
+        {
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = @"delete from ATS_AirportTerminalSettings where ats_id=@ats_id;";
+                    myCommand.Parameters.AddWithValue("@ats_id", ats_id);
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+            }
+        }
+    }
+}

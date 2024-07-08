c30ef287d261b6a5e385394560e7a30eb8ef80d0
Beck Lai <a28282605@gmail.com>
2024-07-08 17:19:58 +0800

    feat 新增價錢連結設定

---
+++ AirportTransferService_API/AirportTransferService/Models/ATS_PriceLineSettings.cs
@@ -0,0 +1,376 @@
+﻿using static AirportTransferService.App_Code.Appsettings;
+
+namespace AirportTransferService.Models
+{
+    /// <summary>
+    /// ATS_PriceLineSettings
+    /// </summary>
+    /// <param name="cre_userid"></param>
+    /// <param name="cre_time"></param>
+    /// <param name="upd_userid"></param>
+    /// <param name="upd_time"></param>
+    /// <param name="pls_id"></param>
+    /// <param name="visible"></param>
+    /// <param name="price"></param>
+    /// <param name="link"></param>
+    public class ATS_PriceLineSettings(
+        string? cre_userid = null,
+        DateTime? cre_time = null,
+        string? upd_userid = null,
+        DateTime? upd_time = null,
+        string? pls_id = null,
+        string? visible = null,
+        decimal? price = null,
+        string? link = null)
+    {
+        /// <summary>
+        /// cre_userid
+        /// </summary>
+        public string? cre_userid { get; } = cre_userid;
+
+        /// <summary>
+        /// cre_time
+        /// </summary>
+        public DateTime? cre_time { get; } = cre_time;
+
+        /// <summary>
+        /// upd_userid
+        /// </summary>
+        public string? upd_userid { get; } = upd_userid;
+
+        /// <summary>
+        /// upd_time
+        /// </summary>
+        public DateTime? upd_time { get; } = upd_time;
+
+        /// <summary>
+        /// pls_id
+        /// </summary>
+        [Key]
+        public string? pls_id { get; } = pls_id;
+
+        /// <summary>
+        /// title
+        /// </summary>
+        public string? visible { get; } = visible;
+
+        /// <summary>
+        /// image
+        /// </summary>
+        public decimal? price { get; } = price;
+
+        /// <summary>
+        /// text1
+        /// </summary>
+        public string? link { get; } = link;
+    }
+
+    /// <summary>
+    /// CreateATS_PriceLineSettingsParam
+    /// </summary>
+    /// <param name="cre_userid"></param>
+    /// <param name="cre_time"></param>
+    /// <param name="visible"></param>
+    /// <param name="price"></param>
+    /// <param name="link"></param>
+    public class CreateATS_PriceLineSettingsParam(
+        string? cre_userid,
+        DateTime? cre_time,
+        string? visible,
+        decimal? price,
+        string? link) : ATS_PriceLineSettings(
+            cre_userid: cre_userid,
+            cre_time: cre_time,
+            visible: visible,
+            price: price,
+            link: link)
+    {
+    }
+
+    /// <summary>
+    /// UpdateATS_PriceLineSettingsParam
+    /// </summary>
+    /// <param name="cre_time"></param>
+    /// <param name="upd_userid"></param>
+    /// <param name="upd_time"></param>
+    /// <param name="pls_id"></param>
+    /// <param name="cre_userid"></param>
+    /// <param name="visible"></param>
+    /// <param name="price"></param>
+    /// <param name="link"></param>
+    public class UpdateATS_PriceLineSettingsParam(
+        DateTime? cre_time,
+        string? upd_userid,
+        DateTime? upd_time,
+        string? pls_id,
+        string? cre_userid = api_string_param_no_pass,
+        string? visible = api_string_param_no_pass,
+        decimal? price = api_numeric_param_no_pass,
+        string? link = api_string_param_no_pass) : ATS_PriceLineSettings(
+            cre_userid: cre_userid,
+            cre_time: cre_time,
+            upd_userid: upd_userid,
+            upd_time: upd_time,
+            pls_id: pls_id,
+            visible: visible,
+            price: price,
+            link: link)
+    {
+    }
+
+    /// <summary>
+    /// SearchATS_PriceLineSettingsParam
+    /// </summary>
+    /// <param name="pls_id"></param>
+    /// <param name="visible"></param>
+    /// <param name="price"></param>
+    /// <param name="link"></param>
+    /// <param name="page"></param>
+    /// <param name="num_per_page"></param>
+    public class SearchATS_PriceLineSettingsParam(
+        string? pls_id = null,
+        string? visible = null,
+        decimal? price = null,
+        string? link = null,
+        int page = 0,
+        int num_per_page = 0)
+    {
+        /// <summary>
+        /// pls_id
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_PriceLineSettings.pls_id")]
+        public string? pls_id { get; } = pls_id;
+
+        /// <summary>
+        /// visible
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Equal, "ATS_PriceLineSettings.visible")]
+        public string? visible { get; } = visible;
+
+        /// <summary>
+        /// price
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_PriceLineSettings.price")]
+        public decimal? price { get; } = price;
+
+        /// <summary>
+        /// link
+        /// </summary>
+        [SQLSearchCondition(SQLSearchConditionType.Like, "ATS_PriceLineSettings.link")]
+        public string? link { get; } = link;
+
+        /// <summary>
+        /// page
+        /// </summary>
+        public int page { get; } = page;
+
+        /// <summary>
+        /// num_per_page
+        /// </summary>
+        public int num_per_page { get; } = num_per_page;
+    }
+
+    /// <summary>
+    /// SearchATS_PriceLineSettingsResult
+    /// </summary>
+    public class SearchATS_PriceLineSettingsResult : IEquatable<SearchATS_PriceLineSettingsResult>
+    {
+        /// <summary>
+        /// cre_userid
+        /// </summary>
+        [SQLSource("ATS_PriceLineSettings.cre_userid")]
+        public string? cre_userid { get; set; }
+
+        /// <summary>
+        /// cre_time
+        /// </summary>
+        [SQLSource("ATS_PriceLineSettings.cre_time")]
+        public DateTime? cre_time { get; set; }
+
+        /// <summary>
+        /// upd_userid
+        /// </summary>
+        [SQLSource("ATS_PriceLineSettings.upd_userid")]
+        public string? upd_userid { get; set; }
+
+        /// <summary>
+        /// upd_time
+        /// </summary>
+        [SQLSource("ATS_PriceLineSettings.upd_time")]
+        public DateTime? upd_time { get; set; }
+
+        /// <summary>
+        /// pls_id
+        /// </summary>
+        [SQLSource("ATS_PriceLineSettings.pls_id")]
+        public string? pls_id { get; set; }
+
+        /// <summary>
+        /// visible
+        /// </summary>
+        [SQLSource("ATS_PriceLineSettings.visible")]
+        public string? visible { get; set; }
+
+        /// <summary>
+        /// price
+        /// </summary>
+        [SQLSource("ATS_PriceLineSettings.price")]
+        public decimal? price { get; set; }
+
+        /// <summary>
+        /// link
+        /// </summary>
+        [SQLSource("ATS_PriceLineSettings.link")]
+        public string? link { get; set; }
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="searchATS_PriceLineSettingsResult"></param>
+        /// <returns></returns>
+        public bool Equals(SearchATS_PriceLineSettingsResult? searchATS_PriceLineSettingsResult)
+        {
+            if (searchATS_PriceLineSettingsResult is null) return false;
+
+            if (Object.ReferenceEquals(this, searchATS_PriceLineSettingsResult)) return true;
+
+            return
+                cre_userid == searchATS_PriceLineSettingsResult.cre_userid &&
+                cre_time == searchATS_PriceLineSettingsResult.cre_time &&
+                upd_userid == searchATS_PriceLineSettingsResult.upd_userid &&
+                upd_time == searchATS_PriceLineSettingsResult.upd_time &&
+                pls_id == searchATS_PriceLineSettingsResult.pls_id &&
+                visible == searchATS_PriceLineSettingsResult.visible &&
+                price == searchATS_PriceLineSettingsResult.price &&
+                link == searchATS_PriceLineSettingsResult.link;
+        }
+    }
+
+    #region API
+    /// <summary>
+    /// ATS_PriceLineSettingsCreate
+    /// </summary>
+    public class ATS_PriceLineSettingsCreate
+    {
+        /// <summary>
+        /// 是否可見
+        /// </summary>
+        [Display(Name = "是否可見"), YN]
+        public string? visible { get; set; } = "Y";
+
+        /// <summary>
+        /// 價錢
+        /// </summary>
+        [Display(Name = "價錢"), Required(ErrorMessage = "請輸入{0}")]
+        public decimal? price { get; set; } = 0;
+
+        /// <summary>
+        /// 連結
+        /// </summary>
+        [Display(Name = "連結")]
+        public string? link { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_PriceLineSettingsUpdate
+    /// </summary>
+    public class ATS_PriceLineSettingsUpdate
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號"), Required(ErrorMessage = "請輸入{0}")]
+        public string? pls_id { get; set; } = "";
+
+        /// <summary>
+        /// 是否可見
+        /// </summary>
+        [Display(Name = "是否可見")]
+        public string? visible { get; set; } = api_string_param_no_pass;
+
+        /// <summary>
+        /// 價錢
+        /// </summary>
+        [Display(Name = "價錢")]
+        public decimal? price { get; set; } = api_numeric_param_no_pass;
+
+        /// <summary>
+        /// 連結
+        /// </summary>
+        [Display(Name = "連結")]
+        public string? link { get; set; } = api_string_param_no_pass;
+    }
+
+    /// <summary>
+    /// ATS_PriceLineSettingsSearch
+    /// </summary>
+    public class ATS_PriceLineSettingsSearch
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號")]
+        public string? pls_id { get; set; } = "";
+
+        /// <summary>
+        /// 頁碼
+        /// </summary>
+        [Display(Name = "頁碼")]
+        public int page { get; set; } = 0;
+
+        /// <summary>
+        /// 一頁幾筆
+        /// </summary>
+        [Display(Name = "一頁幾筆")]
+        public int num_per_page { get; set; } = 10;
+
+        /// <summary>
+        /// 是否匯出
+        /// </summary>
+        [Display(Name = "是否匯出"), YN]
+        public string excel { get; set; } = "N";
+
+        /// <summary>
+        /// 是否可見
+        /// </summary>
+        [Display(Name = "是否可見"), YN]
+        public string? visible { get; set; } = "N";
+
+        /// <summary>
+        /// 價錢
+        /// </summary>
+        [Display(Name = "價錢")]
+        public decimal? price { get; set; } = 0;
+
+        /// <summary>
+        /// 連結
+        /// </summary>
+        [Display(Name = "連結")]
+        public string? link { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_PriceLineSettingsSearchResult
+    /// </summary>
+    public class ATS_PriceLineSettingsSearchResponse : ATS_PriceLineSettingsCreate
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號")]
+        public string? pls_id { get; set; } = "";
+    }
+
+    /// <summary>
+    /// ATS_PriceLineSettingsDelete
+    /// </summary>
+    public class ATS_PriceLineSettingsDelete
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Display(Name = "編號"), Required(ErrorMessage = "請輸入{0}")]
+        public string? pls_id { get; set; } = "";
+    }
+    #endregion
+}
---
+++ AirportTransferService_API/AirportTransferService/Services/IATS_PriceLineSettings.cs
@@ -0,0 +1,195 @@
+﻿namespace AirportTransferService.Services
+{
+    /// <summary>
+    /// IATS_PriceLineSettings
+    /// </summary>
+    public interface IATS_PriceLineSettings
+    {
+        /// <summary>
+        /// CreateATS_PriceLineSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        string CreateATS_PriceLineSettings(CreateATS_PriceLineSettingsParam param);
+
+        /// <summary>
+        /// SearchATS_PriceLineSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <param name="columns"></param>
+        /// <param name="sort_columns"></param>
+        /// <param name="page_count"></param>
+        /// <returns></returns>
+        List<SearchATS_PriceLineSettingsResult> SearchATS_PriceLineSettings(SearchATS_PriceLineSettingsParam param, List<string> columns, List<SQL.SQLOrder_obj> sort_columns, out int page_count);
+
+        /// <summary>
+        /// UpdateATS_PriceLineSettings
+        /// </summary>
+        /// <param name="param"></param>
+        void UpdateATS_PriceLineSettings(UpdateATS_PriceLineSettingsParam param);
+
+        /// <summary>
+        /// DeleteATS_PriceLineSettings
+        /// </summary>
+        /// <param name="pls_id"></param>
+        void DeleteATS_PriceLineSettings(string pls_id);
+    }
+
+    /// <summary>
+    /// IATS_PriceLineSettings_IMPL
+    /// </summary>
+    public class IATS_PriceLineSettings_IMPL : IATS_PriceLineSettings
+    {
+        /// <summary>
+        /// _config
+        /// </summary>
+        public readonly IConfiguration _config;
+        private readonly string strConn = "";
+
+        /// <summary>
+        /// IATS_PriceLineSettings_IMPL
+        /// </summary>
+        /// <param name="config"></param>
+        public IATS_PriceLineSettings_IMPL(IConfiguration config)
+        {
+            _config = config;
+            strConn = _config["sql_conn"];
+        }
+
+        /// <summary>
+        /// CreateATS_PriceLineSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public string CreateATS_PriceLineSettings(CreateATS_PriceLineSettingsParam param)
+        {
+            SQL.GenerateSQLCreateQuery(param, out string str_column, out string str_value, false);
+
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                ResultObject<string> get_auto_res = SQL.GetAutoNumber(myConn, "ATS_PriceLineSettings", "", 99999999, false);
+                if (!get_auto_res.success) throw new Exception(message: "取得流水號失敗");
+                string pls_id = get_auto_res.data ?? "";
+                if (string.IsNullOrEmpty(pls_id)) throw new Exception(message: "Empty Key");
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = $@"
+                    insert into ATS_PriceLineSettings({str_column})
+                    values({str_value});";
+                    myCommand.Parameters.AddWithValue("@pls_id", pls_id);
+                    foreach (var property in param.GetType().GetProperties())
+                    {
+                        if (property.GetValue(param) != null) myCommand.Parameters.AddWithValue($"@{property.Name}", property.GetValue(param));
+                    }
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+
+                return pls_id;
+            }
+        }
+
+        /// <summary>
+        /// SearchATS_PriceLineSettings
+        /// </summary>
+        /// <param name="param"></param>
+        /// <param name="columns"></param>
+        /// <param name="sort_columns"></param>
+        /// <param name="page_count"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public List<SearchATS_PriceLineSettingsResult> SearchATS_PriceLineSettings(SearchATS_PriceLineSettingsParam param, List<string> columns, List<SQL.SQLOrder_obj> sort_columns, out int page_count)
+        {
+            if (!SQL.CheckSearchColumn<SearchATS_PriceLineSettingsResult>(columns)) throw new Exception(message: "Search Column Not Exists");
+
+            DataTable dt = new();
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                string strSql = $@"
+                select {SQL.GenerateSQLSelectQuery<SearchATS_PriceLineSettingsResult>(columns)}
+                from ATS_PriceLineSettings
+                where 1=1
+                {SQL.GenerateSQLWhereQuery(param)}
+                {SQL.GenerateSQLOrderQuery<SearchATS_PriceLineSettingsResult>(sort_columns, "order by ATS_PriceLineSettings.pls_id desc")}
+                {(param.page > 0 ? "offset((@page-1)) * @num_per_page ROWS fetch next @num_per_page ROWS only;" : "")}";
+
+                dt = SQL.GenerateSQLSelectResult(param, strSql, myConn, out page_count);
+            }
+
+            List<SearchATS_PriceLineSettingsResult> result = [];
+            foreach (DataRow dr in dt.Rows)
+            {
+                result.Add(new SearchATS_PriceLineSettingsResult
+                {
+                    cre_userid = dt.Columns.Contains("cre_userid") ? dr["cre_userid"].ToString() : null,
+                    cre_time = dt.Columns.Contains("cre_time") ? dr.Field<DateTime?>("cre_time") : null,
+                    upd_userid = dt.Columns.Contains("upd_userid") ? dr["upd_userid"].ToString() : null,
+                    upd_time = dt.Columns.Contains("upd_time") ? dr.Field<DateTime?>("upd_time") : null,
+                    pls_id = dt.Columns.Contains("pls_id") ? dr["pls_id"].ToString() : null,
+                    visible = dt.Columns.Contains("visible") ? dr["visible"].ToString() : null,
+                    price = dt.Columns.Contains("price") ? dr.Field<decimal>("price") : null,
+                    link = dt.Columns.Contains("link") ? dr["link"].ToString() : null
+                });
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// UpdateATS_PriceLineSettings
+        /// </summary>
+        /// <param name="param"></param>
+        public void UpdateATS_PriceLineSettings(UpdateATS_PriceLineSettingsParam param)
+        {
+            SQL.GenerateSQLUpdateQuery(param, out string str);
+            SQL.GenerateSQLColumnLogQuery(param, "ATS_PriceLineSettings", out string column_log_output_str, out string column_log_insert_str, out string tmp_table_create_str);
+
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = $@"
+                    {tmp_table_create_str}
+                    update ATS_PriceLineSettings
+                    {str}
+                    {column_log_output_str}
+                    where pls_id=@pls_id
+                    {column_log_insert_str}";
+                    foreach (var property in param.GetType().GetProperties())
+                    {
+                        myCommand.Parameters.AddWithValue($"@{property.Name}", property.GetValue(param) ?? DBNull.Value);
+                    }
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+            }
+        }
+
+        /// <summary>
+        /// DeleteATS_PriceLineSettings
+        /// </summary>
+        /// <param name="pls_id"></param>
+        public void DeleteATS_PriceLineSettings(string pls_id)
+        {
+            using (SqlConnection myConn = new(strConn))
+            {
+                myConn.Open();
+
+                using (SqlCommand myCommand = new("", myConn))
+                {
+                    myCommand.CommandText = @"delete from ATS_PriceLineSettings where pls_id=@pls_id;";
+                    myCommand.Parameters.AddWithValue("@pls_id", pls_id);
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Cancel();
+                }
+            }
+        }
+    }
+}

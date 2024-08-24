6fd153a78eafb4a617d455fc945c1ef993ca8e09
Beck Lai <a28282605@gmail.com>
2024-08-24 23:47:34 +0800

    feat 移除公司資訊

--- AirportTransferService_WEB/src/components/System/SysFooter.js
+++ AirportTransferService_WEB/src/components/System/SysFooter.js
@@ -8,42 +8,42 @@ const theme = createTheme(CusThemeTS.defaultTheme);
  * 版權宣告
  */
 const Copyright = () => {
-  const [version, setVersion] = useState('1.2.4');
-  const [year, setYear] = useState(new Date().getFullYear());
+    const [version, setVersion] = useState('1.2.4');
+    const [year, setYear] = useState(new Date().getFullYear());
 
-  return (
-    <Grid container sx={{
-      position: 'absolute',
-      bottom: 0,
-      height: '30px',
-      alignItems: "center",
-      background: theme.footer.backgroundColor
-    }}>
-      <Grid item xs={8} md={8} sx={{
-        pr: 1,
-        pl: 1,
-        justifyContent: "start",
-        display: { xs: "none", md: "flex" },
-      }}>
-        <Typography sx={{ color: theme.palette.primary.dark }}>
-          <span>{"Copyright © " + year + " "}</span>
-          <a target="_blank" href="https://www.genesys-tech.com/" style={{ color: theme.palette.primary.dark }}>Genesys Technology Ltd.</a> All rights reserved.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4} sx={{
-        pr: 1,
-        pl: 1,
-        justifyContent: { xs: "center", md: "end" },
-        display: "flex",
-      }}>
-        <Typography sx={{ color: theme.palette.primary.dark }}>
-          Made By
+    return (
+        <Grid container sx={{
+            position: 'absolute',
+            bottom: 0,
+            height: '30px',
+            alignItems: "center",
+            background: theme.footer.backgroundColor
+        }}>
+            <Grid item xs={8} md={8} sx={{
+                pr: 1,
+                pl: 1,
+                justifyContent: "start",
+                display: { xs: "none", md: "flex" },
+            }}>
+                <Typography sx={{ color: theme.palette.primary.dark }}>
+                    {/* <span>{"Copyright © " + year + " "}</span>
+          <a target="_blank" href="https://www.genesys-tech.com/" style={{ color: theme.palette.primary.dark }}>Genesys Technology Ltd.</a> All rights reserved. */}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} md={4} sx={{
+                pr: 1,
+                pl: 1,
+                justifyContent: { xs: "center", md: "end" },
+                display: "flex",
+            }}>
+                <Typography sx={{ color: theme.palette.primary.dark }}>
+                    {/* Made By
           <a target="_blank" href="https://www.genesys-tech.com/" style={{ color: theme.palette.primary.dark }}> Genesys Technology Ltd. </a>
-          <span>ver {version}　</span>
-        </Typography>
-      </Grid>
-    </Grid>
-  )
+          <span>ver {version}　</span> */}
+                </Typography>
+            </Grid>
+        </Grid>
+    )
 }
 
 export { Copyright };

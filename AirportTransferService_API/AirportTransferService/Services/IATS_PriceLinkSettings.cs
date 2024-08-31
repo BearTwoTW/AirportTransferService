c29c29fc746b3680cafc0b9518c160e28f0397b1
Beck Lai <a28282605@gmail.com>
2024-09-01 02:28:16 +0800

    feat 價錢連結加入接送機類別

--- AirportTransferService_API/AirportTransferService/Services/IATS_PriceLinkSettings.cs
+++ AirportTransferService_API/AirportTransferService/Services/IATS_PriceLinkSettings.cs
@@ -116,7 +116,7 @@
                 from ATS_PriceLinkSettings
                 where 1=1
                 {SQL.GenerateSQLWhereQuery(param)}
-                {SQL.GenerateSQLOrderQuery<SearchATS_PriceLinkSettingsResult>(sort_columns, "order by ATS_PriceLinkSettings.pls_id desc")}
+                {SQL.GenerateSQLOrderQuery<SearchATS_PriceLinkSettingsResult>(sort_columns, "order by ATS_PriceLinkSettings.price,ATS_PriceLinkSettings.type desc")}
                 {(param.page > 0 ? "offset((@page-1)) * @num_per_page ROWS fetch next @num_per_page ROWS only;" : "")}";
 
                 dt = SQL.GenerateSQLSelectResult(param, strSql, myConn, out page_count);
@@ -133,6 +133,7 @@
                     upd_time = dt.Columns.Contains("upd_time") ? dr.Field<DateTime?>("upd_time") : null,
                     pls_id = dt.Columns.Contains("pls_id") ? dr["pls_id"].ToString() : null,
                     visible = dt.Columns.Contains("visible") ? dr["visible"].ToString() : null,
+                    type = dt.Columns.Contains("type") ? dr["type"].ToString() : null,
                     price = dt.Columns.Contains("price") ? dr.Field<decimal>("price") : null,
                     link = dt.Columns.Contains("link") ? dr["link"].ToString() : null
                 });

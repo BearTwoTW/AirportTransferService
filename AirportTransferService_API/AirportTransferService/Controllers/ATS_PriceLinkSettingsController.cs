97b56dbd90b6c6017775d148506264ff335bf558
Beck Lai <a28282605@gmail.com>
2024-09-23 01:40:00 +0800

    feat 新增城市、區域欄位

--- AirportTransferService_API/AirportTransferService/Controllers/ATS_PriceLinkSettingsController.cs
+++ AirportTransferService_API/AirportTransferService/Controllers/ATS_PriceLinkSettingsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AirportTransferService.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AirportTransferService.Controllers
 {
@@ -7,11 +8,14 @@ namespace AirportTransferService.Controllers
     /// </summary>
     /// <param name="baseService"></param>
     /// <param name="ATS_PriceLinkSettings"></param>
+    /// <param name="aTS_CityAreaSettings"></param>
     public class ATS_PriceLinkSettingsController(
         IBaseService baseService,
-        IATS_PriceLinkSettings ATS_PriceLinkSettings) : CustomControllerBase(baseService)
+        IATS_PriceLinkSettings ATS_PriceLinkSettings,
+        IATS_CityAreaSettings ATS_CityAreaSettings) : CustomControllerBase(baseService)
     {
         private readonly IATS_PriceLinkSettings _ATS_PriceLinkSettings = ATS_PriceLinkSettings;
+        private readonly IATS_CityAreaSettings _ATS_CityAreaSettings = ATS_CityAreaSettings;
 
         /// <summary>
         /// 價錢連結設定建立
@@ -30,11 +34,47 @@ namespace AirportTransferService.Controllers
             }
             #endregion
 
+            //檢查城市區域是否已建立
+            if (!string.IsNullOrEmpty(data.city) || !string.IsNullOrEmpty(data.area))
+            {
+                if (string.IsNullOrEmpty(data.city)) return new ResultObject<string> { success = false, message = "城市不得為空" };
+
+                List<SearchATS_CityAreaSettingsResult> search_cityarea_results = _ATS_CityAreaSettings.SearchATS_CityAreaSettings(
+                    new SearchATS_CityAreaSettingsParam(
+                        city: data.city,
+                        area: data.area),
+                    ["cas_id"], [],
+                    out int page_count);
+                if (search_cityarea_results.Count == 0) return new ResultObject<string> { success = false, message = "城市區域未建立" };
+            }
+
             List<SearchATS_PriceLinkSettingsResult> search_results = _ATS_PriceLinkSettings.SearchATS_PriceLinkSettings(
                 new SearchATS_PriceLinkSettingsParam(),
-                ["type", "price"], [],
+                ["type", "city", "area", "price"], [],
                 out _);
-            if (search_results.Exists(x => x.type == data.type && x.price == data.price)) return new ResultObject<string> { success = false, message = "價錢重複" };
+            if (search_results.Exists(x =>
+            x.type == data.type
+            && ((string.IsNullOrEmpty(x.city) ? "" : x.city) == (string.IsNullOrEmpty(data.city) ? "" : data.city))
+            && ((string.IsNullOrEmpty(x.area) ? "" : x.area) == (string.IsNullOrEmpty(data.area) ? "" : data.area))
+            && x.price == data.price))
+                return new ResultObject<string> { success = false, message = "價錢重複" };
+            // 同一個城市跟區域價錢只會有一種設定
+            if (!string.IsNullOrEmpty(data.city) && !string.IsNullOrEmpty(data.area))
+            {
+                if (search_results.Exists(x =>
+                x.type == data.type
+                && ((string.IsNullOrEmpty(x.city) ? "" : x.city) == (string.IsNullOrEmpty(data.city) ? "" : data.city))
+                && ((string.IsNullOrEmpty(x.area) ? "" : x.area) == (string.IsNullOrEmpty(data.area) ? "" : data.area))))
+                    return new ResultObject<string> { success = false, message = "城市、區域重複" };
+            }
+            else if (!string.IsNullOrEmpty(data.city))
+            {
+                if (search_results.Exists(x =>
+                x.type == data.type
+                && ((string.IsNullOrEmpty(x.city) ? "" : x.city) == (string.IsNullOrEmpty(data.city) ? "" : data.city))
+                && ((string.IsNullOrEmpty(x.area) ? "" : x.area) == "")))
+                    return new ResultObject<string> { success = false, message = "城市重複" };
+            }
 
             string id = _ATS_PriceLinkSettings.CreateATS_PriceLinkSettings(
                 new CreateATS_PriceLinkSettingsParam(
@@ -42,6 +82,8 @@ namespace AirportTransferService.Controllers
                     cre_time: cre_time,
                     visible: data.visible,
                     type: data.type,
+                    city: data.city,
+                    area: data.area,
                     price: data.price,
                     link: data.link));
 
@@ -68,18 +110,58 @@ namespace AirportTransferService.Controllers
             //查自己
             SearchATS_PriceLinkSettingsResult? search_own_result = _ATS_PriceLinkSettings.SearchATS_PriceLinkSettings(
                 new SearchATS_PriceLinkSettingsParam(pls_id: data.pls_id),
-                ["pls_id", "type", "price"], [],
+                ["pls_id", "type", "city", "area", "price"], [],
                 out _).FirstOrDefault();
             if (search_own_result == null) return new ResultObject<string> { success = false, message = "修改失敗，查無價錢連結設定" };
             //查要檢查重複的東西
             List<SearchATS_PriceLinkSettingsResult> search_results = _ATS_PriceLinkSettings.SearchATS_PriceLinkSettings(
                 new SearchATS_PriceLinkSettingsParam(),
-                ["pls_id", "type", "price"], [],
+                ["pls_id", "type", "city", "area", "price"], [],
                 out _);
             string? type = data.type == Appsettings.api_string_param_no_pass ? search_own_result.type : data.type;
+            string? city = data.city == Appsettings.api_string_param_no_pass ? search_own_result.city : data.city;
+            string? area = data.area == Appsettings.api_string_param_no_pass ? search_own_result.area : data.area;
             decimal? price = data.price == Appsettings.api_numeric_param_no_pass ? search_own_result.price : data.price;
             //檢查重複
-            if (search_results.Exists(x => x.type == type && x.price == price && x.pls_id != data.pls_id)) return new ResultObject<string> { success = false, message = "價錢重複" };
+            if (search_results.Exists(x =>
+            x.type == type
+            && ((string.IsNullOrEmpty(x.city) ? "" : x.city) == (string.IsNullOrEmpty(city) ? "" : city))
+            && ((string.IsNullOrEmpty(x.area) ? "" : x.area) == (string.IsNullOrEmpty(area) ? "" : area))
+            && x.price == price && x.pls_id != data.pls_id))
+                return new ResultObject<string> { success = false, message = "價錢重複" };
+            // 同一個城市跟區域價錢只會有一種設定
+            if (!string.IsNullOrEmpty(city) && !string.IsNullOrEmpty(area))
+            {
+                if (search_results.Exists(x =>
+                x.type == type
+                && ((string.IsNullOrEmpty(x.city) ? "" : x.city) == (string.IsNullOrEmpty(city) ? "" : city))
+                && ((string.IsNullOrEmpty(x.area) ? "" : x.area) == (string.IsNullOrEmpty(area) ? "" : area))
+                && x.pls_id != data.pls_id))
+                    return new ResultObject<string> { success = false, message = "城市、區域重複" };
+            }
+            else if (!string.IsNullOrEmpty(city))
+            {
+                if (search_results.Exists(x =>
+                x.type == type
+                && ((string.IsNullOrEmpty(x.city) ? "" : x.city) == (string.IsNullOrEmpty(city) ? "" : city))
+                && ((string.IsNullOrEmpty(x.area) ? "" : x.area) == "")
+                && x.pls_id != data.pls_id))
+                    return new ResultObject<string> { success = false, message = "城市重複" };
+            }
+
+            //檢查城市區域是否已建立
+            if (!string.IsNullOrEmpty(city) || !string.IsNullOrEmpty(area))
+            {
+                if (string.IsNullOrEmpty(city)) return new ResultObject<string> { success = false, message = "城市不得為空" };
+
+                List<SearchATS_CityAreaSettingsResult> search_cityarea_results = _ATS_CityAreaSettings.SearchATS_CityAreaSettings(
+                    new SearchATS_CityAreaSettingsParam(
+                        city: city,
+                        area: area),
+                    ["cas_id"], [],
+                    out int page_count);
+                if (search_cityarea_results.Count == 0) return new ResultObject<string> { success = false, message = "城市區域未建立" };
+            }
 
             using (TransactionScope tx = new())
             {
@@ -90,6 +172,8 @@ namespace AirportTransferService.Controllers
                     pls_id: data.pls_id,
                     visible: data.visible,
                     type: data.type,
+                    city: data.city,
+                    area: data.area,
                     price: data.price,
                     link: data.link));
 
@@ -112,11 +196,13 @@ namespace AirportTransferService.Controllers
                     pls_id: data.pls_id,
                     visible: data.visible,
                     type: data.type,
+                    city: data.city,
+                    area: data.area,
                     price: data.price,
                     link: data.link,
                     page: data.page,
                     num_per_page: data.num_per_page),
-                ["pls_id", "visible", "type", "price", "link"], [],
+                ["pls_id", "visible", "type", "city", "area", "price", "link"], [],
                 out int page_count);
 
             List<ATS_PriceLinkSettingsSearchResponse> response = [];
@@ -127,6 +213,8 @@ namespace AirportTransferService.Controllers
                     pls_id = result.pls_id,
                     visible = result.visible,
                     type = result.type,
+                    city = result.city,
+                    area = result.area,
                     price = result.price,
                     link = result.link
                 });
@@ -138,12 +226,16 @@ namespace AirportTransferService.Controllers
                 DataTable dt_excel = new();
                 dt_excel.Columns.Add("價錢", typeof(decimal));
                 dt_excel.Columns.Add("接/送機", typeof(string));
+                dt_excel.Columns.Add("城市", typeof(string));
+                dt_excel.Columns.Add("區域", typeof(string));
                 dt_excel.Columns.Add("連結", typeof(string));
                 foreach (ATS_PriceLinkSettingsSearchResponse obj in response)
                 {
                     DataRow dr_excel = dt_excel.NewRow();
                     dr_excel["價錢"] = obj.price;
                     dr_excel["接/送機"] = obj.type;
+                    dr_excel["城市"] = obj.city;
+                    dr_excel["區域"] = obj.area;
                     dr_excel["連結"] = obj.link;
                     dt_excel.Rows.Add(dr_excel);
                 }
